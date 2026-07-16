2aa5a7706d55eb1f4da8d2838b665de7bce68fbd
Yukita1226 <thammarat.songwiwat@gmail.com>
2026-07-16 14:45:40 +0700

    connect c3 to random

--- 4knee_project/instruction.md
+++ 4knee_project/instruction.md
@@ -1,1 +1,14 @@
-1. how to set up (set on ardunio )
+knee instruction 
+
+code at https://github.com/Yukita1226/elec_work/tree/main/4knee_project
+
+
+1.backend (ewsp32 c3)
+
+    - use esp32c3 dev module
+
+    - before connect it to com hold boot button and then connect com or else it will connect and disconnect constantly
+
+    -you need to change uint8_t receiverMac at back_c3.ino" if you change esp32 you can upload checkaddress.ino" into new esp32 to get address
+
+    - backend file name : "4knee_project\back_c3\back_c3.ino"
--- 4knee_project/screen/screen.ino
+++ 4knee_project/screen/screen.ino
@@ -1,1 +1,46 @@
-hi
+#include <esp_now.h>
+#include <WiFi.h>
+
+typedef struct struct_message {
+
+  float knee1;
+  float knee2;
+  float knee3;
+  float knee4;
+
+} struct_message;
+
+struct_message knee;
+
+void onDataRecv(const uint8_t *mac, const uint8_t *data, int len) {
+  memcpy(&knee, data, sizeof(knee));
+
+  Serial.print("From: ");
+  for (int i = 0; i < 6; i++) {
+    Serial.printf("%02X", mac[i]);
+    if (i < 5) Serial.print(":");
+  }
+  Serial.println();
+
+  Serial.printf("knee1: %.2f, knee2: %.2f, knee3: %.2f, knee4: %.2f\n",
+                knee.knee1, knee.knee2, knee.knee3, knee.knee4);
+}
+
+void setup() {
+  
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA); 
+
+  Serial.print("Receiver MAC: ");
+  Serial.println(WiFi.macAddress());
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("ESP-NOW init failed");
+    return;
+  }
+
+  esp_now_register_recv_cb(onDataRecv);
+}
+
+void loop() {
+}
